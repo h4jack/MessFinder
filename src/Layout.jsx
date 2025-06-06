63a46181f04fecaaecb1f6fdefb4a0427cccf2a1
h4jack <susantamandi.dev@gmail.com>
2025-06-06 17:40:22 +0530

    implemented the scrollToTop whenever it is needed

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -4,7 +4,7 @@ import { Header, Footer } from './components/layout'
 import {
   Outlet,
 } from "react-router-dom";
-import { ScrollToTop } from './scroll-to-top';
+import ScrollToTop from './ScrollToTop';
 
 const Layout = () => {
   const user = {
